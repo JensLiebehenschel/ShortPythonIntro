feee264dd513a37ab6a385d55f8dfe69a5b0a492
Edward Späth <edward5000@gmx.de>
2023-03-27 14:09:30 +0200

    Trying to add correct gif

--- ShortPythonIntro/_build/jupyter_execute/Interaktivitaet.py
+++ ShortPythonIntro/_build/jupyter_execute/Interaktivitaet.py
@@ -5,9 +5,26 @@
 
 # ## **DIESE PYTHON EINFÜHRUNG IST WORK-IN-PROGRESS. ES KÖNNEN NOCH ÄNDERUNGEN VORGENOMMEN WERDEN.**
 
-# TODO: Erklären wie Interaktivität mit Thebe funktioniert (sobald es funktioniert)...
+# ## Interaktivität aktivieren
 
-# Um die Interaktivität zu testen, können Sie den unten stehenden Bereich nutzen um Code zu schreiben und auszuführen.
+# In dieser Einführung können Sie interaktiv kleine Programmieraufgaben lösen.
+# Sie können Code schreiben und diesen ohne die Webseite verlassen zu müssen, ausführen lassen. 
+# So können Sie einerseits ihr Ergebnis überprüfen, andererseits aber auch gegebenenfalls durch Fehlernachrichten dazulernen. 
+# 
+# Dafür müssen sie lediglich die Interaktivität aktivieren.
+# 
+# Nicht jede Seite ist interaktiv. Interaktive Seiten erkennen Sie an der Rakete über der Überschrift. Bewegen Sie ihre Maus über die Rakete und drücken sie auf den Knopf <code>Live Code</code>. 
+# Bereiche wo sie Code schreiben können, erkennen Sie an den drei Knöpfen <code>run</code>, <code>restart</code> und <code>restart &amp; run all</code>. Bei dieser Einführung brauchen Sie nur den Knopf <code>run</code>. Die restlichen Knöpfe können Sie ignorieren.
+# 
+# <IMG SRC="https://raw.githubusercontent.com/JensLiebehenschel/ShortPythonIntro/main/ShortPythonIntro/interaktivitaet_aktivieren.gif">
+
+# ## Interaktivität ausprobieren
+
+# Um die Interaktivität auszuprobieren, können Sie den unten stehenden Bereich nutzen um Code zu schreiben und auszuführen. Ersetzen Sie etwa den Bereich wo Sie schreiben können durch die Zeile
+# 
+# ```Python
+# print("Hello World!")
+# ```
 
 # In[1]:
 
